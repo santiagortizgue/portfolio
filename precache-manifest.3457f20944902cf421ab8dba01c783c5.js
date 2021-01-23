923dc4963240ec4006203353eccc6c542042a39b
SantiagoOrtiz0630 <san.or.gue@gmail.com>
2021-01-22 23:15:38 -0500

    Updates

---
+++ precache-manifest.3457f20944902cf421ab8dba01c783c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eb87620c9a10c82053dda3abe3b4fea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f54cde2ceb0c4e017e6",
+    "url": "/static/css/2.caf03aba.chunk.css"
+  },
+  {
+    "revision": "631cd80d49b3d05e19b0",
+    "url": "/static/css/main.34247606.chunk.css"
+  },
+  {
+    "revision": "6f54cde2ceb0c4e017e6",
+    "url": "/static/js/2.4a3a441c.chunk.js"
+  },
+  {
+    "revision": "631cd80d49b3d05e19b0",
+    "url": "/static/js/main.10556357.chunk.js"
+  },
+  {
+    "revision": "58d901bf3a38ad5969be",
+    "url": "/static/js/runtime-main.bc985581.js"
+  }
+]);
